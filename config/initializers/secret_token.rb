8213e89c9ad466c9c2a9b007d4a093fdde227de8
nandkishore <nandkishore.rails@gmail.com>
2014-08-13 21:09:54 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockMirror::Application.config.secret_token = '49cb780c3e771c0a266230e167c5ada57dbac9f6c08d17d3b9fb446246f29e4983579924dc1c0e789095c1dd2579a6b323f4103f00bfd964e50de860d9fadaf7'
