97a5c659dcd611535243028875206768676eaeab
nandkishore <nandkishore.rails@gmail.com>
2014-08-13 22:56:14 +0530

    implement views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,19 @@
 class WelcomeController < ApplicationController
 	def home
-		
+	end
+
+	def about_us
+	end
+
+	def services
+	end
+
+	def pricing
+	end
+
+	def contact_us
+	end
+
+	def careers
 	end	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -48,6 +48,11 @@ StockMirror::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+   match 'about_us' => 'welcome#about_us'
+   match 'services' => 'welcome#services'
+   match 'pricing' => 'welcome#pricing'
+   match 'contact_us' => 'welcome#contact_us'
+   match 'careers' => 'welcome#careers'
    root :to => 'welcome#home'
 
   # See how all your routes lay out with "rake routes"
